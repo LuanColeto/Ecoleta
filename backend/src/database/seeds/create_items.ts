b9189ea42168fa6e4f59203cff2b7ba1ecdefe1e
Luan Coleto <luancoleto@outlook.com>
2020-06-03 11:25:23 -0300

    Seeds

---
+++ backend/src/database/seeds/create_items.ts
@@ -0,0 +1,12 @@
+import Knex from 'knex'
+
+export async function seed(knex:Knex) {
+   await knex('items').insert([
+        { title: 'Lâmpadas', image: 'lampada.svg '},
+        { title: 'Baterias e Pilhas', image: 'baterias.svg '},
+        { title: 'Papéis e Papelão', image: 'papeis-papelao.svg '},
+        { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg '},
+        { title: 'Resíduos orgânicos', image: 'organicos.svg '},
+        { title: 'Óleode cozinha', image: 'oleo.svg '},
+    ])
+}
