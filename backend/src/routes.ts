d80e5ae8cd720b40d02154dd8cd23faa655eca3a
Luan Coleto <luancoleto@outlook.com>
2020-06-03 11:27:19 -0300

    Creating routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,23 @@
-import express from 'express';
+import express, {response} from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json("HELLO WORLD")
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+      return {
+          id: item.id,
+          name: item.title,
+          image_url: `http://localhost:3333/uploads/${item.image}`
+      }
+
+    });
+
+    return response.json(serializedItems);
 });
 
+routes.post('/points', );
 
 export default routes;
