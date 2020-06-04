c3dcf37c34911f3ac1373433f5c097b5fec065c2
Luan Coleto <luancoleto@outlook.com>
2020-06-03 22:47:58 -0300

    Cleaning files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Header from './header'
 
 function App() {
+
+  let [counter, setCounter] = useState(1111);
+
+  function onClickButton() {
+    setCounter(counter )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div>
+          <Header title="Hello World" />
+
+          <h3>{counter}</h3>
+          <button onClick={onClickButton}>Aumentar</button>
+        </div>
   );
 }
 
