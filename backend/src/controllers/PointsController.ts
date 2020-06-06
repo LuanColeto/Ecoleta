a144597ed056b90ead4ab69f6d1253d9348abab7
Luan Coleto <luancoleto@outlook.com>
2020-06-06 19:58:22 -0300

    Update PointsController.ts

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -4,6 +4,29 @@ import knex from "../database/connection";
 
 class PointsController {
 
+    async index(request: Request, response: Response) {
+        const {
+            city,
+            uf,
+            items
+        } = request.query;
+
+        const parsedItems = String(items)
+            .split(',')
+            .map((item) => Number(item.trim()));
+
+        const points = await knex('points')
+            .join('point_items', 'points.id', '=', 'point_items.point_id')
+            .whereIn('point_items.item_id', parsedItems)
+            .where('city', String(city))
+            .where('uf', String(uf))
+            .distinct()
+            .select('points.*');
+
+        return response.json(points);
+
+    }
+
     async show(request: Request, response: Response) {
         const { id } = request.params;
 
@@ -19,29 +42,6 @@ class PointsController {
             .select('items.title');
 
         return response.json({point, items})
-    }
-
-    async index(request: Request, response: Response) {
-        const {
-            city,
-            uf,
-            items
-        } = request.query;
-
-        const parsedItems = String(items)
-            .split(',')
-            .map(item => Number(item.trim()));
-
-        const points = await     knex('points')
-            .join('point_items', 'points.id', '=', 'point_items.point_id')
-            .whereIn('point_items.item_id', parsedItems)
-            .where('city', String(city))
-            .where('uf', String(uf))
-            .distinct()
-            .select('points.*');
-
-        return response.json(points);
-
     }
 
     async create(request: Request, response: Response) {
